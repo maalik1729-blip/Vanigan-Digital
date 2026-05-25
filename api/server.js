30aacf56ece523d0ccf89dea8d21853e76bcf6e2
maalik1729-blip <maalik1729@gmail.com>
2026-05-25 10:58:37 +0530

    fix: set outputDirectory to dist/client for vercel SPA deployment

--- api/server.js
+++
@@ -1,9 +0,0 @@
-import server from "../dist/server/server.js";
-
-export const config = {
-  runtime: "edge",
-};
-
-export default async function handler(request, context) {
-  return server.fetch(request, null, context);
-}
